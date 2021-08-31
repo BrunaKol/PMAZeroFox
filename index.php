eac57e3207097830f49abee95781a3866003c6af
BrunaKol <kolakbruna@gmail.com>
2021-08-31 20:45:17 +0200

    update

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ include("zerofoxdb.php");
 $upit = "SELECT * FROM korisnik";
 
 $rezultat = mysqli_query($konekcija, $upit);
+// print(mysqli_error($konekcija));
 var_dump($rezultat);
 
 $redak = mysqli_fetch_assoc($rezultat);
